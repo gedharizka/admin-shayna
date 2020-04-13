e55a0c91ecf357171fc6bcc2e68f6eb6d57174e4
Gedha Rizka <gedharizka@gmail.com>
2020-04-13 22:16:11 +0700

    index transaction

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('products/{id}/gallery','ProductController@gallery')->name('products.gallery');
 Route::resource('products', 'ProductController');
 Route::resource('product-galleries', 'ProductGalleryController');
+Route::resource('transactions', 'TransactionController');
